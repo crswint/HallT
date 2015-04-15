d7740a6f19cef934f29383c0dc477b8e8b86dfdb
crswin5726 <crswin5626@ung.edu>
2015-04-15 11:24:32 -0400

    Added Templates and Content
    Added templates for all 8 individual routes as well as a template to show all the routes. Added content to base (to be inherited from), as well as content to main (styling, nav bar, etc). Connected all templates with individual views and urls. Added content to the all routes template to serve as a place holder and build off of for the other route templates.

--- apps/tran_maps/urls.py
+++ apps/tran_maps/urls.py
@@ -8,5 +8,14 @@ urlpatterns = patterns('',
 
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^main/', views.MainView.as_view()),
+    url(r'^onenorth/', views.OneNorth.as_view(), name='onenorth'),
+    url(r'^onesouth/', views.OneSouth.as_view(), name='onesouth'),
+    url(r'^two/', views.Two.as_view(), name='two'),
+    url(r'^three/', views.Three.as_view(), name='three'),
+    url(r'^four/', views.Four.as_view(), name='four'),
+    url(r'^fivea/', views.FiveA.as_view(), name='fivea'),
+    url(r'^fiveb/', views.FiveB.as_view(), name='fiveb'),
+    url(r'^six/', views.Six.as_view(), name='six'),
+    url(r'^all_routes/', views.AllRoutes.as_view(), name='all_routes'),
 
 )
--- apps/tran_maps/views.py
+++ apps/tran_maps/views.py
@@ -7,3 +7,48 @@ from django.views import generic
 class MainView(generic.TemplateView):
     """Loads the main page."""
     template_name = 'tran_maps/main.html'
+
+
+class OneNorth(generic.TemplateView):
+    """Loads the Route 1 North Page"""
+    template_name = 'tran_maps/onenorth.html'
+
+
+class OneSouth(generic.TemplateView):
+    """Loads the Route 1 South Page"""
+    template_name = 'tran_maps/onesouth.html'
+
+
+class Two(generic.TemplateView):
+    """Loads the Route 2 Page"""
+    template_name = 'tran_maps/two.html'
+
+
+class Three(generic.TemplateView):
+    """Loads the Route 3 Page"""
+    template_name = 'tran_maps/three.html'
+
+
+class Four(generic.TemplateView):
+    """Loads the Route 4 Page"""
+    template_name = 'tran_maps/four.html'
+
+
+class FiveA(generic.TemplateView):
+    """Loads the Route 5 A Page"""
+    template_name = 'tran_maps/fivea.html'
+
+
+class FiveB(generic.TemplateView):
+    """Loads the Route 5 B Page"""
+    template_name = 'tran_maps/fiveb.html'
+
+
+class Six(generic.TemplateView):
+    """Loads the Route 6 Page"""
+    template_name = 'tran_maps/six.html'
+
+
+class AllRoutes(generic.TemplateView):
+    """Loads All the Routes on one Page"""
+    template_name = 'tran_maps/all_routes.html'
