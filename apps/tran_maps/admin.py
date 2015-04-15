fdc96d90a4417a7606c142bb32d1780dc409ef29
crswin5726 <crswin5626@ung.edu>
2015-04-15 10:33:54 -0400

    Created Two models and Registered Admin
    I created two models, Stops and Routes to hold the two types of data associated with the transit system. I also registered the models in the admin file so they can be seen and edited in the admin page.

--- apps/tran_maps/admin.py
+++ apps/tran_maps/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from apps.tran_maps import models
 # Register your models here.
+# we want to see this model in the admin page, this accomplishes that
+
+admin.site.register(models.Stop)
+admin.site.register(models.Route)
+
--- apps/tran_maps/models.py
+++ apps/tran_maps/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+
+class Stop(models.Model):
+    """This model will hold public transit bus stop information"""
+    geom = models.PointField(srid=4326)
+    name = models.CharField(max_length=40)
+    amenities = models.CharField(max_length=40)
+
+
+class Route(models.Model):
+    """This model will hold public transit bus routes information"""
+    geom = models.LineStringField(srid=4326)
+    route_num = models.CharField(max_length=40)
+    stops = models.ManyToManyField(Stop)
